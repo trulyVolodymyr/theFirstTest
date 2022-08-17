f63c274fdbbe4650ddbc969ff0552a60945feed2
vladimir vychikov <vovaorc@MacBook-Pro-Vychikov.local>
2022-08-17 18:57:55 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <TheHeader />
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+export default {
+    components: {
+        TheHeader,
+    },
+}
+</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,6 @@
+<template>
+    <header class="header">
+        <div class="header_lang"></div>
+        <div class="header_logo"></div>
+    </header>
+</template>
